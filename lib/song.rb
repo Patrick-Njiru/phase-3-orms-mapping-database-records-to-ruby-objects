956387bd63c1bd9f224c5929c536dbb13006f1c5
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-11-03 16:18:41 +0300

    Completed assignment.

--- lib/song.rb
+++ lib/song.rb
@@ -49,4 +49,20 @@ class Song
     song.save
   end
 
+  def self.new_from_db(row)
+    #row comes in form of an array
+    # self.new == Song.new
+    self.new(id: row[0], name: row[1], album: row[2])
+  end
+
+  def self.all
+    DB[:conn].execute("SELECT * FROM songs")
+    .map { |row| self.new_from_db(row) }
+  end
+
+  def self.find_by_name(name)
+    DB[:conn].execute("SELECT * FROM songs WHERE name = ? LIMIT 1", name)
+    .map { |row| self.new_from_db(row) }
+    .first # picks first element in an array.
+  end
 end
